b367d7b4380e4ec8354e854701b0de06274adeaf
kasztp <kasztp@gmail.com>
2021-03-08 00:20:22 +0100

    New feature: Sudoku checking for solvability. Design changes on documentation page. Limited input checking.

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,20 @@ from app import app
 from .solver.sudoku_solver import Board
 
 
+def parse_payload(data):
+    size = int(sqrt(len(data["payload"])))
+    box_size = int(sqrt(size))
+    dimensions = (1, size + 1)
+    board = []
+    row = []
+    for i, item in enumerate(data["payload"]):
+        row.append(int(item))
+        if (i + 1) % size == 0:
+            board.append(row)
+            row = []
+    return Board(board, size, box_size, dimensions)
+
+
 @app.route('/')
 def root():
     return render_template('index.html', title='SudokuAPI Documentation')
@@ -12,36 +26,63 @@ def root():
 @app.route('/v1/solve', methods=["GET", "POST"])
 def solve():
     data = request.get_json(silent=True)
-    board = []
-    row = []
-    for i, item in enumerate(data["payload"]):
-        row.append(int(item))
-        if (i + 1) % 9 == 0:
-            board.append(row)
-            row = []
-    size = int(sqrt(len(data["payload"])))
-    box_size = int(sqrt(sqrt(len(data["payload"]))))
-    dimensions = (1, 10)
-    challenge = Board(board, size, box_size, dimensions)
-    passes = challenge.preprocess_board()
-    challenge.solve()
-    solution = ''
-    for row in challenge.board:
-        for number in row:
-            solution += (str(number))
-    response = {
-        "original": data["payload"],
-        "solved": solution,
-        "iterations": challenge.iterations,
-        "passes": passes
-    }
-    return response
+    if not sqrt(len(data["payload"])).is_integer():
+        response = {
+            "error": "Invalid input length or non square board size.",
+            "original": data["payload"],
+            "solvable": False
+        }
+        return response, 400
+    challenge = parse_payload(data)
+    solvable = challenge.check_solvable()
+    if not solvable:
+        response = {
+            "error": "Invalid clues.",
+            "original": data["payload"],
+            "solvable": solvable
+        }
+        return response, 400
+    else:
+        passes = challenge.preprocess_board()
+        challenge.solve()
+        solution = ''
+        for row in challenge.board:
+            for number in row:
+                solution += (str(number))
+        response = {
+            "original": data["payload"],
+            "solved": solution,
+            "iterations": challenge.iterations,
+            "passes": passes
+        }
+        return response
 
 
 @app.route('/v1/check', methods=["GET", "POST"])
 def check():
-    pass
-    # TODO To be implemented
+    data = request.get_json(silent=True)
+    if not sqrt(len(data["payload"])).is_integer():
+        response = {
+            "original": data["payload"],
+            "solvable": False,
+            "reason": "Invalid input length or non square board size."
+        }
+    else:
+        challenge = parse_payload(data)
+        solvable = challenge.check_solvable()
+        if solvable:
+            response = {
+                "original": data["payload"],
+                "solvable": solvable,
+                "reason": "Looks good."
+            }
+        else:
+            response = {
+                "original": data["payload"],
+                "solvable": solvable,
+                "reason": "Invalid clues."
+            }
+    return response
 
 
 @app.route('/v1/generate', methods=["GET", "POST"])
