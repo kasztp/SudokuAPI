9176aaeb89611bff0fb2f9440d5193b6033cab02
Peter Kaszt <kasztp@gmail.com>
2021-12-11 22:56:53 +0100

    Fix Issue: #1 and #2

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from math import sqrt
-from flask import abort, render_template, request
+from flask import abort, escape, render_template, request
+from random import randint
 from app import app
 from .solver.sudoku_solver import Board
 
@@ -75,24 +76,25 @@ def check():
     data = request.get_json(silent=True)
     if not request.json or 'payload' not in request.json:
         abort(400)
-    if not sqrt(len(data["payload"])).is_integer():
+    submitted = escape(data["payload"])
+    if not sqrt(len(submitted)).is_integer():
         response = {
-            "original": data["payload"],
+            "original": submitted,
             "solvable": False,
             "reason": "Invalid input length or non square board size."
         }
     else:
-        challenge = parse_payload(data)
+        challenge = parse_payload(submitted)
         solvable = challenge.check_solvable()
         if solvable:
             response = {
-                "original": data["payload"],
+                "original": submitted,
                 "solvable": solvable,
                 "reason": "Looks good."
             }
         else:
             response = {
-                "original": data["payload"],
+                "original": submitted,
                 "solvable": solvable,
                 "reason": "Invalid clues."
             }
@@ -101,5 +103,37 @@ def check():
 
 @app.route('/v1/generate', methods=["GET"])
 def generate():
-    # TODO To be implemented
+    # Initial version - to be improved & optimized
+    size = 9
+    box_size = int(sqrt(size))
+    dimensions = (1, size + 1)
+    board = []
+    row = []
+    solvable = False
+    iterations = 0
+    while not solvable:
+        iterations += 1
+        for i in range(90):
+            number = randint(0, 9)
+            if number not in row:
+                row.append(number)
+            else:
+                row.append(0)
+            if (i + 1) % size == 0:
+                board.append(row)
+                row = []
+        print(board)
+        temp_board = Board(board, size, box_size, dimensions)
+        print(temp_board)
+        if temp_board.check_solvable():
+            solvable = True
+            challenge = ''
+            for row in temp_board.board:
+                for number in row:
+                    challenge += (str(number))
+            response = {
+                "sudoku": challenge,
+                "iterations": iterations,
+            }
+            return response
     pass
